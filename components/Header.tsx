fcd2b58eeb7af095fe77175fe9d5f246f9bbbcbf
Husen <husenikary@mail.ru>
2025-02-03 20:25:48 +0500

    fich added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,25 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Header = () => {
+
+    const pathname = usePathname()
+
   return (
-    <div>Header</div>
+    <header className='my-10 flex justify-between gap-5'>
+        <Link href={'/'}>BookWise</Link>
+
+        <ul className='flex flex-row items-center gap-8'>
+            <li>
+                <Link href={'/library'} className='text-base cursor-pointer capitalize'>
+                    Library
+                </Link>
+            </li>
+        </ul>
+    </header>
   )
 }
 
