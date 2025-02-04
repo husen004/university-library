15a6e0cfc688d2ee4557feb2094b744fbeb4f94b
Husen <husenikary@mail.ru>
2025-02-04 14:27:42 +0500

    new icon

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ const layout = ({ children }: {children: ReactNode}) => {
     <main className='root-container'>
         <div className='mx-auto max-w-7xl'>
             <Header />
-        <div className='mt-20 pb-20'>{children}</div>
+        {children}
         </div>
     </main>
   )
