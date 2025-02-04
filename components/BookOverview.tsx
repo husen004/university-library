f262f73d6da1867159c21123090779bb1def9fdb
Husen <husenikary@mail.ru>
2025-02-04 21:36:49 +0500

    props added

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const BookOverview = ({  }) => {
+const BookOverview = ({ title, author, genre, rating, total, copies, available_copies, description, color, cover }) => {
   return (
     <section className='book-overview'>
         <div className='flex flex-1 flex-col gap-5'>
