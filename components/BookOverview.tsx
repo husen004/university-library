b23cda6466b45e95966bdd613a5548500ec05da7
Husen <husenikary@mail.ru>
2025-02-04 18:59:51 +0500

    change

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const BookOverview = () => {
   return (
-    <div>BookOverview</div>
+    <section className='book-overview'>
+        <div className='flex flex-1 flex-col gap-5'>
+            <h1 className=''>A Long Book Title</h1>
+        </div>
+    </section>
   )
 }
 
