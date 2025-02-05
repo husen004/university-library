e7090c2bcf623b7e1b27a232bc9e48c402420a94
Husen <husenikary@mail.ru>
2025-02-05 19:09:33 +0500

    BookCoverVariant type added

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
 
+type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide"
+
+const variantStyles: Record<BookCoverVariant, string> = {
+    extraSmall: "book-cover_extra-small",
+    small: "book-cover_small",
+    medium: "book-cover_medium",
+    regular: "book-cover_regular",
+    wide: "book-cover_wide",
+}
+
 interface Props {
     className?: string
-    variant: 'wide' | 'small'
+    variant: BookCoverVariant,
     coverColor: string
     coverImage: string
 }
 
 const BookCover = ({ variant, className, coverColor, coverImage }: Props) => {
   return (
-    <div>BookCover</div>
+    <div>
+
+    </div>
   )
 }
 
