6594bc7d9430568bcee54d7a2231b20f196daa1f
Husen <husenikary@mail.ru>
2025-02-06 11:01:42 +0500

    config change

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -16,20 +16,20 @@ interface Props {
     className?: string
     variant: BookCoverVariant,
     coverColor: string
-    coverUrl: string
+    coverImage: string
 }
 
 const BookCover = ({ 
   className, 
   variant = "regular", 
   coverColor = "#012B48", 
-  coverUrl = "https://placehold.co/400x600.png" }: Props) => {
+  coverImage = "https://placehold.co/400x600.png" }: Props) => {
   return (
     <div className={cn("relative transition-all duration-300", variantStyles[variant], className)}>
       BOOK SIDE SVG
       
       <div className='absolute z-10' style={{left: "12%", width: "87.5%", height: "88%"}}>
-        <Image src={coverUrl} alt="Book cover" fill className='rounded-sm object-fill'  />
+        <Image src={coverImage} alt="Book cover" fill className='rounded-sm object-fill'  />
       </div>
     </div>
   )
