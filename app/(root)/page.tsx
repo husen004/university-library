c05224625356178500702b4fb5c424b496e4b529
Husen <husenikary@mail.ru>
2025-02-08 21:32:42 +0500

    change

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,9 +5,9 @@ import { sampleBooks } from '@/constants'
 const Home = () => (
   <>
     <BookOverview isLoanedBook={false} {...sampleBooks[0]} />
-
     <BookList title="Latest" books={sampleBooks} containerClassName="mt-28" />
   </>
 )
+
 
 export default Home
