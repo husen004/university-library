ea19e7bf2eb2614f98053f98efbfcbf469bb4da6
Husen <husenikary@mail.ru>
2025-02-10 17:25:23 +0500

    photo illustration added

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,11 +6,19 @@ const layout = ({ children }: { children: ReactNode }) => {
     <main className='auth-container'>
         <section className='auth-form'>
           <div className='auth-box'>
-            <Image src={'/icons/logo.svg'} alt='logo' width={37} height={37} />
-            <h1 className='text-2xl font-semibold text-white'>BookWise</h1>
-          </div>
 
-          <div>{children}</div>
+            <div className='flex flex-row gap-3'>
+              <Image src={'/icons/logo.svg'} alt='logo' width={37} height={37} />
+              <h1 className='text-2xl font-semibold text-white'>BookWise</h1>
+            </div>
+
+            <div>{children}</div>
+
+          </div>
+        </section>
+
+        <section className='auth-illustration'>
+          <Image src={'/images/auth-illustration.png'} alt='auth illustration' width={1000} height={1000} />
         </section>
     </main>
   )
