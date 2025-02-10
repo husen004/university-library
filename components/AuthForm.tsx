9e6f2649b89ed7f7c745867bbf45135735b03670
Husen <husenikary@mail.ru>
2025-02-10 17:31:12 +0500

    form added

---
+++ components/AuthForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// interface Props<T extends FieldValues> {}
+
+const AuthForm = ({ type, schema, defaultValues, onSubmit }) => {
+  return (
+    <div>AuthForm</div>
+  )
+}
+
+export default AuthForm
