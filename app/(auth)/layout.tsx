0187b9f76cc276513d64fccd35bdad55107e55c6
Husen <husenikary@mail.ru>
2025-02-10 17:16:57 +0500

    auth change

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,16 @@
+import Image from 'next/image'
 import React, { ReactNode } from 'react'
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-        {children}
-    </div>
+    <main className='auth-container'>
+        <section className='auth-form'>
+          <div className='auth-box'>
+            <Image src={'/icons/logo.svg'} alt='logo' width={37} height={37} />
+            <h1 className='text-2xl font-semibold text-white'>BookWise</h1>
+          </div>
+        </section>
+    </main>
   )
 }
 
