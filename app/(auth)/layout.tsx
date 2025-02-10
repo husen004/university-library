e6b23c3daf7f913655f62c4ee7e4c57e74e4bfee
Husen <husenikary@mail.ru>
2025-02-10 17:18:02 +0500

    post

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,6 +9,8 @@ const layout = ({ children }: { children: ReactNode }) => {
             <Image src={'/icons/logo.svg'} alt='logo' width={37} height={37} />
             <h1 className='text-2xl font-semibold text-white'>BookWise</h1>
           </div>
+
+          <div>{children}</div>
         </section>
     </main>
   )
