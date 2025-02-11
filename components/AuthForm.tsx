fe6200f0fbef0429bf713edd734f37805c14ef8f
Husen <husenikary@mail.ru>
2025-02-11 21:07:33 +0500

    import for input added

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,29 +1,48 @@
-"use client"
+"use client";
 
-import { zodResolver } from '@hookform/resolvers/zod'
-import React from 'react'
-import { DefaultValues, FieldValues, SubmitHandler, useForm, UseFormReturn } from 'react-hook-form'
-import { ZodType } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import {
+  DefaultValues,
+  FieldValues,
+  SubmitHandler,
+  useForm,
+  UseFormReturn,
+} from "react-hook-form";
+import { ZodType } from "zod";
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 interface Props<T extends FieldValues> {
   scheme: ZodType<T>;
   onSubmit: (data: T) => Promise<{ success: boolean; error: string }>;
   type: "SIGN_IN" | "SIGN_UP";
-
 }
 
-const AuthForm = <T extends FieldValues> ({ type, schema, defaultValues, onSubmit }: Props<T>) => {
-
+const AuthForm = <T extends FieldValues>({
+  type,
+  schema,
+  defaultValues,
+  onSubmit,
+}: Props<T>) => {
   const form: UseFormReturn<T> = useForm({
     resolver: zodResolver(schema),
-    defaultValues: defaultValues as DefaultValues<T>
+    defaultValues: defaultValues as DefaultValues<T>,
   });
 
-  const handleSubmit: SubmitHandler<T> = async ( data ) => {}
+  const handleSubmit: SubmitHandler<T> = async (data) => {};
 
-  return (
-    <div>AuthForm  -- {type}</div>
-  )
-}
+  return <div>AuthForm -- {type}</div>;
+};
 
-export default AuthForm
+export default AuthForm;
