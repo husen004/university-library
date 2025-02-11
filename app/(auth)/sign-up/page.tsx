cdb619c141d4c25174ebc7b57ab0cda6cd1f38fd
Husen <husenikary@mail.ru>
2025-02-11 20:51:05 +0500

    sign in up confugure

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,21 @@
+import AuthForm from '@/components/AuthForm'
+import { signUpSchema } from '@/lib/validation'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Sign Up Form</div>
+    <AuthForm
+    type="SIGN_UP"
+    schema={signUpSchema}
+    defaultValues={{
+        email: "",
+        password: "",
+        fullName: "",
+        universityId: 0,
+        universityCard: "",
+    }}
+    onSubmit={() => {}}
+    />
   )
 }
 
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const AuthForm = ({ type, schema, defaultValues, onSubmit }) => {
   return (
-    <div>AuthForm</div>
+    <div>AuthForm  -- {type}</div>
   )
 }
 
