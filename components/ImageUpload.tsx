0a5709d85703985e4237f568f13728673d1e76b1
Husen <husenikary@mail.ru>
2025-02-15 16:43:37 +0500

    imagekitio added to dep

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { IKImage, IKVideo, ImageKitProvider, IKUpload, ImageKitContext } from "imagekitio-next";
+
 const ImageUpload = () => {
   return (
     <div>ImageUpload</div>
