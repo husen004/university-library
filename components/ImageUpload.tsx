2e1e046a465061069b7083b7138b2e0539adc34e
Husen <husenikary@mail.ru>
2025-02-16 13:22:11 +0500

    error handler added

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -5,8 +5,24 @@ import {
   ImageKitProvider,
   IKUpload,
 } from "imagekitio-next";
+import config from "@/lib/config";
 
+const authentocator = async () => {
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`)
 
+    if(!response.ok) {
+      const errorText = await response.text();
+
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      )
+    }
+
+  } catch (error: any) {
+    throw new Error(`Authentication request failed: ${error.massage}`)
+  }
+}
 
 const ImageUpload = () => {
   return <div>ImageUpload</div>;
--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@ const config = {
     env: {
         apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT,
         imagekit: {
-            publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
+            publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
             urlEndpoint: process.env.NEXT_PUBLIC_IMAGE_KEY,
             privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
         }
