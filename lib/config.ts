c4fcde042bebc798776bddac16ed9b735e81041d
Husen <husenikary@mail.ru>
2025-02-20 14:00:33 +0500

    config fixed

--- lib/config.ts
+++ lib/config.ts
@@ -3,7 +3,7 @@ const config = {
         apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
         imagekit: {
             publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
-            urlEndpoint: process.env.NEXT_PUBLIC_IMAGE_KEY!,
+            urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
             privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
         }
     }
