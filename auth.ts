f0f48299cdbc91c36e99e7ee40bac632ef61e3a7
Husen <husenikary@mail.ru>
2025-02-24 20:56:43 +0500

    auth setud new added

--- auth.ts
+++ auth.ts
@@ -1,5 +1,27 @@
-import NextAuth from "next-auth"
- 
+import NextAuth from "next-auth";
+import { db } from "./database/drizzle";
+import { eq } from "drizzle-orm";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-})
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialProvider({
+      async authorize(credentials) {
+        if (!credetials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await db
+          .select()
+          .from(users)
+          .where(eq("email", credentials.email.toString()));
+      },
+    }),
+  ],
+});
+
+function CredentialProvider(arg0: {}): import("@auth/core/providers").Provider {
+  throw new Error("Function not implemented.");
+}
