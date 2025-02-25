a27b112eeeab8f659f64106a4d7b84ebc4bb3383
Husen <husenikary@mail.ru>
2025-02-25 20:30:44 +0500

    sign up done

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,11 +1,35 @@
 "use server";
 
+import { signIn } from "@/auth";
 import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 import bcrypt, { hash } from "bcryptjs";
 import { eq } from "drizzle-orm";
 
-const signUp = async (params: AuthCredentials) => {
+export const signInWithCredentials = async (
+  params: Pick<AuthCredentials, "email" | "password">
+) => {
+  const { email, password } = params;
+
+  try {
+    const result = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (result?.error) {
+      return { success: false, massage: result.error };
+    }
+
+    return { success: true };
+  } catch (error) {
+    console.log(error, "Signin error");
+    return { success: false, massage: "Signin failed" };
+  }
+};
+
+export const signUp = async (params: AuthCredentials) => {
   const { fullName, email, password, universityId, universityCard } = params;
 
   // checks if the user already exists
@@ -15,24 +39,26 @@ const signUp = async (params: AuthCredentials) => {
     .where(eq(users.email, email))
     .limit(1);
 
-    if(existingUser.length > 0) {
-      return { success: false, massage: "User already exist" };
-    }
+  if (existingUser.length > 0) {
+    return { success: false, massage: "User already exist" };
+  }
 
-    const hashpassword = await hash(password, 10);
+  const hashpassword = await hash(password, 10);
 
-    try{
-        await db.insert(users).values({
-            fullName,
-            email,
-            universityId,
-            password: hashpassword,
-            universityCard,
-        })
+  try {
+    await db.insert(users).values({
+      fullName,
+      email,
+      universityId,
+      password: hashpassword,
+      universityCard,
+    });
 
-        // await signInWithCredentials({ email, password });
-    } catch (error) {
-        console.log(error, "Signup error")
-        return { success: false, massage: "Signup failed" }
-    }
+    // await signInWithCredentials({ email, password });
+
+    return { success: true };
+  } catch (error) {
+    console.log(error, "Signup error");
+    return { success: false, massage: "Signup failed" };
+  }
 };
