d7c5544f5ddb31633b9d77cf4721b693e14609c2
Husen <husenikary@mail.ru>
2025-02-27 21:16:22 +0500

    Sign out profile added

--- app/(root)/my-profile/page.tsx
+++ app/(root)/my-profile/page.tsx
@@ -1,5 +1,8 @@
-import { signOut } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { signOut } from "@/auth";
+import BookList from "@/components/BookList";
+import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
+;
 import React from "react";
 
 const page = () => {
@@ -10,12 +13,13 @@ const page = () => {
           "use server";
 
           await signOut();
-          redirect("/sign-in");
         }}
         className="mb-10"
       >
-        <button className="">Logout</button>
+        <Button className="">Logout</Button>
       </form>
+
+      <BookList title="Borrowed Books" books={sampleBooks} />
     </>
   );
 };
