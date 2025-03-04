9f7bc08a160e738a31cfcffa67130c326cf66146
Husen <husenikary@mail.ru>
2025-03-04 16:19:06 +0500

    side bar setup

--- components/admin/SideBar.tsx
+++ components/admin/SideBar.tsx
@@ -1,8 +1,41 @@
+import { adminSideBarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <div className='admin-sidebar'>
+        <div className=''>
+            <div className='logo'>
+                <Image
+                src="/icons/admin/logo.svg"
+                alt='logo'
+                width={37}
+                height={37}
+                />
+                <h1 className=''>BookWise</h1>
+            </div>
+
+            <div className='mt-10 flex flex-col gap-5'>
+                {adminSideBarLinks.map((link) => {
+                    const isSelected: boolean = false
+
+                    return (
+                        <Link href={link.route} key={link.route}>
+                            <div className={cn("link", isSelected && "bg-primary-admin shadow-sm")}>
+                                <div className='relative size-5'>
+                                    <Image src={link.img} alt='link-icon' width={30} height={30}/>
+                                    <p className=''>{link.text}</p>
+                                </div>
+                            </div>
+                        </Link>
+                    )
+                })}
+            </div>
+        </div>
+    </div>
   )
 }
 
