bede153ca8fbcd5e9b35b967ad92300e9242a1e1
Husen <husenikary@mail.ru>
2025-03-11 14:06:41 +0500

    new db for image upload

--- database/seed.ts
+++ database/seed.ts
@@ -1,21 +1,65 @@
-import dummyBooks from "../dummybooks.json"
+import ImageKit from "imagekit";
+import dummyBooks from "../dummybooks.json";
+import { books } from "./schema";
+import { config } from "dotenv";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
-const uploadToImageKit = (url: string, fileName: string, folder: string) => {
-    
-}
+config({ path: ".env.local" })
+
+const sql = neon(process.env.DATABASE_URL!)
+export const db = drizzle({client: sql}) 
+
+const imagekit = new ImageKit({
+  publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
+  urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+});
+
+const uploadToImageKit = async (
+  url: string,
+  fileName: string,
+  folder: string
+) => {
+  try {
+    const response = await imagekit.upload({
+      file: url,
+      fileName,
+      folder,
+    });
+
+    return response.filePath
+  } catch (error) {
+    console.error("Error uploading imgae to ImageKit", error);
+  }
+};
 
 const seed = async () => {
   console.log("Seeding data...");
 
   try {
     for (const book of dummyBooks) {
-        const coverUrl = await uploadToImageKit(
-          book.coverUrl,
-          `${book.title}`,
-          "/books/covers"
-        );
-      }
+      const coverUrl = await uploadToImageKit(
+        book.coverUrl,
+        `${book.title}.jpg`,
+        "/books/covers"
+      );
+
+      const videoUrl = await uploadToImageKit(
+        book.videoUrl,
+        `${book.title}.mp4`,
+        "/books/videos"
+      );
+
+      await db.insert(books).values({
+        ...book,
+        coverUrl,
+        videoUrl,
+      })
+    }
+
+    console.log("Data seeded successfully!")
   } catch (error) {
-    console.error("Error seeding data", error)
+    console.error("Error seeding data", error);
   }
 };
