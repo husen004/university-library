aa318bcbdbd6c71c2bf9eabad6e722cc962f186b
Husen <husenikary@mail.ru>
2025-03-12 15:20:59 +0500

    video checker create

--- app/(root)/books/[id]/page.tsx
+++ app/(root)/books/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/auth";
 import BookOverview from "@/components/BookOverview";
+import BookVideo from "@/components/BookVideo";
 import { db } from "@/database/drizzle";
 import { books } from "@/database/schema";
 import { eq } from "drizzle-orm";
@@ -7,8 +8,8 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
-    const id = (await params).id;
-    const session = await auth();
+  const id = (await params).id;
+  const session = await auth();
 
   const [bookDetails] = await db
     .select()
@@ -24,20 +25,25 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
       <div className="book-details">
         <div className="flex-[1.5]">
-            <section className="flex flex-col gap-7">
-                <h3>Summary</h3>
+          <section className="flex flex-col gap-7">
+            <h3>Video</h3>
 
-                <div className="space-y-5 text-xl text-light-100">
-                    {bookDetails.summary.split("\n").map((line, i) => (
-                        <p key={i}>{line}</p>
-                    ))}
-                </div>
-            </section>
+            <BookVideo videoUrl={bookDetails.videoUrl} />
+
+          </section>
+
+          <section className="mt-10 flex flex-col gap-7">
+            <h3>Summary</h3>
+
+            <div className="space-y-5 text-xl text-light-100">
+              {bookDetails.summary.split("\n").map((line, i) => (
+                <p key={i}>{line}</p>
+              ))}
+            </div>
+          </section>
         </div>
 
         {/* Similar Book */}
-
-        
       </div>
     </>
   );
